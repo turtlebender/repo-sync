b0f59c0dfa4d9297f4e406949b04b0cf5a0f7666
Tom Howe <turtlebender@gmail.com>
2012-02-27 16:27:54 +0000

    adding reposync script

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(name='reposync',
+        zip_safe=False,
         version='1.0a',
         author='Tom Howe',
         author_email='turtlebender@gmail.com',
-        packages=['.'],
+        packages=['reposync'],
         install_requires=[ 'pyinotify' ],
-        scripts=[ 'scripts/repopush', 'scripts/repopull' ]
+        scripts=[ 'scripts/repopush', 'scripts/repopull', 'scripts/reposync' ]
         )
